a7492f3b24758b91c9a9da67c9f615064b9fc7de
Adrien MARION <adr.marion@outlook.com>
2024-03-12 14:15:36 +0100

    feat: react native vision camera

--- app.config.ts
+++ app.config.ts
@@ -7,5 +7,22 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "Picts Manager",
   extra: {
     apiUrl: process.env.API_URL ?? "http://localhost:4000",
+    eas: {
+      projectId: "e9810629-0500-42f0-b2df-15b69854d4bd",
+    },
+  },
+  plugins: [
+    [
+      "react-native-vision-camera",
+      {
+        cameraPermissionText: "$(PRODUCT_NAME) needs access to your Camera.",
+      },
+    ],
+  ],
+  android: {
+    package: "com.epitech.pictsmanager",
+  },
+  ios: {
+    bundleIdentifier: "com.epitech.pictsmanager",
   },
 });

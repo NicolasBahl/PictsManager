3d1f38c37113496054b0348f26f845a80a3a2c1c
HugiSimon <h.simon@ludus-academie.com>
2024-03-25 11:07:06 +0100

    feat: Add select mode and context menu to album details

--- app/(app)/albumDetails.tsx
+++ app/(app)/albumDetails.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, FlatList, Image, Dimensions } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import SearchBar from '@/components/SearchBar';
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
+import { ContextMenuButton } from 'react-native-ios-context-menu';
 
 function AlbumDetails() {
   const { album } = useLocalSearchParams();
@@ -15,22 +16,149 @@ function AlbumDetails() {
     'https://imgupscaler.com/images/samples/animal-after.webp',
     'https://kinsta.com/fr/wp-content/uploads/sites/4/2020/09/jpeg.jpg'
   ];
+  const [selectedImages, setSelectedImages] = useState<string[]>([]);
+  const [isSelectMode, setIsSelectMode] = useState(false);
 
   const numColumns = 3;
   const imageSize = (Dimensions.get('window').width * 0.9 - (numColumns + 1) * 4) / numColumns;
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.back()} style={styles.back}>
-        <Ionicons name="chevron-back" size={30} color="#467599" />
-        <Text style={styles.backText}>All albums</Text>
-      </TouchableOpacity>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()} style={styles.back}>
+          <Ionicons name="chevron-back" size={30} color="#467599" />
+          <Text style={styles.backText}>All albums</Text>
+        </TouchableOpacity>
+        <ContextMenuButton
+          style={{ position: 'absolute', right: 0 }}
+          menuConfig={isSelectMode ? {
+            menuTitle: '',
+            menuItems: [{
+              actionKey: 'selectAll',
+              actionTitle: selectedImages.length === images.length ? 'Unselect All' : 'Select All',
+              icon: {
+                type: 'IMAGE_SYSTEM',
+                imageValue: {
+                  systemName: 'checkmark',
+                },
+              },
+            }, {
+              actionKey: 'delete',
+              actionTitle: 'Delete',
+              icon: {
+                type: 'IMAGE_SYSTEM',
+                imageValue: {
+                  systemName: 'trash',
+                },
+              },
+            }, {
+              actionKey: 'cancel',
+              actionTitle: 'Cancel',
+              icon: {
+                type: 'IMAGE_SYSTEM',
+                imageValue: {
+                  systemName: 'xmark',
+                },
+              },
+            }],
+          } : {
+            menuTitle: '',
+            menuItems: [{
+              actionKey: 'select',
+              actionTitle: 'Select',
+              icon: {
+                type: 'IMAGE_SYSTEM',
+                imageValue: {
+                  systemName: 'checkmark',
+                },
+              },
+            }, {
+              menuTitle: 'Sort',
+              icon: {
+                type: 'IMAGE_SYSTEM',
+                imageValue: {
+                  systemName: 'arrow.up.arrow.down',
+                },
+              },
+              menuItems: [{
+                actionKey: 'sort-date-asc',
+                actionTitle: 'Date Asc',
+                icon: {
+                  type: 'IMAGE_SYSTEM',
+                  imageValue: {
+                    systemName: 'calendar',
+                  },
+                }
+              }, {
+                actionKey: 'sort-date-desc',
+                actionTitle: 'Date Desc',
+                icon: {
+                  type: 'IMAGE_SYSTEM',
+                  imageValue: {
+                    systemName: 'calendar.badge.minus',
+                  },
+                }
+              }]
+            }],
+          }}
+          onPressMenuItem={({ nativeEvent }) => {
+            switch (nativeEvent.actionKey) {
+              case 'select':
+                setIsSelectMode(true);
+                break;
+              case 'selectAll':
+                if (selectedImages.length === images.length) {
+                  setSelectedImages([]);
+                } else {
+                  setSelectedImages(images);
+                }
+                break;
+              case 'delete':
+                setSelectedImages([]);
+                setIsSelectMode(false);
+                break;
+              case 'cancel':
+                setSelectedImages([]);
+                setIsSelectMode(false);
+                break;
+              case 'sort-date-asc':
+                break;
+              case 'sort-date-desc':
+                break;
+            }
+          }}
+        >
+          <Ionicons name="ellipsis-horizontal-circle" size={28} color="#467599" />
+        </ContextMenuButton>
+      </View>
       <Text style={styles.titleAlbum}>{album}</Text>
       <SearchBar />
       <FlatList
         data={images}
         renderItem={({ item }) => (
-          <Image source={{ uri: item }} style={{ ...styles.image, width: imageSize, height: imageSize }} />
+          <View
+            onTouchEnd={() => {
+              if (isSelectMode) {
+                setSelectedImages(prev => {
+                  if (prev.includes(item)) {
+                    return prev.filter(i => i !== item);
+                  } else {
+                    return [...prev, item];
+                  }
+                });
+              }
+            }}
+          >
+            <Image source={{ uri: item }} style={{ ...styles.image, width: imageSize, height: imageSize }} />
+            {isSelectMode && (
+              <Ionicons
+                name={selectedImages.includes(item) ? "checkmark-circle" : "ellipse-outline"}
+                size={24}
+                color="#467599"
+                style={styles.imageIcon}
+              />
+            )}
+          </View>
         )}
         keyExtractor={item => item}
         numColumns={numColumns}
@@ -44,9 +172,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  back: {
+  header: {
     marginTop: 60,
-    paddingLeft: 16,
+    width: "90%",
+    alignSelf: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  back: {
     flexDirection: "row",
   },
   backText: {
@@ -65,6 +198,11 @@ const styles = StyleSheet.create({
   imageContainer: {
     alignSelf: 'center',
     marginTop: 8,
+  },
+  imageIcon: {
+    position: 'absolute',
+    top: 4,
+    right: 4,
   },
   image: {
     margin: 2,
